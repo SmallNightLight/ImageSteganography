da2f41bd2afa77cda5244eb29b81ea5ced9158c8
SmallNightLight <matthiaskruis@gmail.com>
2024-03-17 18:20:00 +0100

    C# project Init

---
+++ Source/DataEmbedderLSB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ImageSteganography
+{
+    public class DataEmbedderLSB
+    {
+        public void Enocode()
+        {
+
+        }
+
+        public void Decode()
+        {
+
+        }
+    }
+}
