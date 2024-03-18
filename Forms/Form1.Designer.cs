3f25802735d2c92b0fc9023ab9f25ad4b6d73e29
SmallNightLight <matthiaskruis@gmail.com>
2024-03-18 17:10:20 +0100

    Open File Dialog for encoding and decoding

--- Forms/Form1.Designer.cs
+++ Forms/Form1.Designer.cs
@@ -28,110 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
-            encodingPath = new TextBox();
-            button1 = new Button();
-            button2 = new Button();
-            encodeMessage = new TextBox();
-            textBox3 = new TextBox();
-            label1 = new Label();
-            label2 = new Label();
+            EncodingPath = new TextBox();
+            EncodeButton = new Button();
+            DecodeButton = new Button();
+            EncodingMessage = new TextBox();
+            DecodingPath = new TextBox();
+            EncodingLabel = new Label();
+            DecodingLabel = new Label();
+            DecodingMessage = new TextBox();
+            OpenEncodeFile = new Button();
+            OpenDecodeFile = new Button();
+            EncodeImageName = new Label();
+            DecodeImageName = new Label();
             SuspendLayout();
             // 
-            // encodingPath
+            // EncodingPath
             // 
-            encodingPath.AcceptsTab = true;
-            encodingPath.Location = new Point(178, 125);
-            encodingPath.Name = "encodingPath";
-            encodingPath.PlaceholderText = "path...";
-            encodingPath.Size = new Size(196, 23);
-            encodingPath.TabIndex = 0;
-            encodingPath.TabStop = false;
-            encodingPath.TextChanged += textBox1_TextChanged;
+            EncodingPath.AcceptsTab = true;
+            EncodingPath.Location = new Point(184, 126);
+            EncodingPath.Name = "EncodingPath";
+            EncodingPath.PlaceholderText = "path...";
+            EncodingPath.Size = new Size(166, 23);
+            EncodingPath.TabIndex = 0;
+            EncodingPath.TabStop = false;
             // 
-            // button1
+            // EncodeButton
             // 
-            button1.Location = new Point(299, 198);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 1;
-            button1.Text = "Encode";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            EncodeButton.Location = new Point(305, 311);
+            EncodeButton.Name = "EncodeButton";
+            EncodeButton.Size = new Size(75, 23);
+            EncodeButton.TabIndex = 1;
+            EncodeButton.Text = "Encode";
+            EncodeButton.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // DecodeButton
             // 
-            button2.Location = new Point(407, 198);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 2;
-            button2.Text = "Decode";
-            button2.UseVisualStyleBackColor = true;
+            DecodeButton.Location = new Point(413, 311);
+            DecodeButton.Name = "DecodeButton";
+            DecodeButton.Size = new Size(75, 23);
+            DecodeButton.TabIndex = 2;
+            DecodeButton.Text = "Decode";
+            DecodeButton.UseVisualStyleBackColor = true;
             // 
-            // encodeMessage
+            // EncodingMessage
             // 
-            encodeMessage.Location = new Point(178, 154);
-            encodeMessage.Multiline = true;
-            encodeMessage.Name = "encodeMessage";
-            encodeMessage.PlaceholderText = "message...";
-            encodeMessage.Size = new Size(196, 38);
-            encodeMessage.TabIndex = 3;
-            encodeMessage.TextChanged += textBox2_TextChanged;
+            EncodingMessage.Location = new Point(184, 193);
+            EncodingMessage.Multiline = true;
+            EncodingMessage.Name = "EncodingMessage";
+            EncodingMessage.PlaceholderText = "message...";
+            EncodingMessage.Size = new Size(196, 112);
+            EncodingMessage.TabIndex = 3;
             // 
-            // textBox3
+            // DecodingPath
             // 
-            textBox3.Location = new Point(407, 125);
-            textBox3.Name = "textBox3";
-            textBox3.PlaceholderText = "path...";
-            textBox3.Size = new Size(196, 23);
-            textBox3.TabIndex = 4;
-            textBox3.TextChanged += textBox3_TextChanged;
+            DecodingPath.Location = new Point(413, 126);
+            DecodingPath.Name = "DecodingPath";
+            DecodingPath.PlaceholderText = "path...";
+            DecodingPath.Size = new Size(165, 23);
+            DecodingPath.TabIndex = 4;
             // 
-            // label1
+            // EncodingLabel
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 12F);
-            label1.Location = new Point(242, 90);
-            label1.Name = "label1";
-            label1.Size = new Size(74, 21);
-            label1.TabIndex = 5;
-            label1.Text = "Encoding";
-            label1.Click += label1_Click;
+            EncodingLabel.AutoSize = true;
+            EncodingLabel.Font = new Font("Segoe UI", 12F);
+            EncodingLabel.Location = new Point(242, 90);
+            EncodingLabel.Name = "EncodingLabel";
+            EncodingLabel.Size = new Size(74, 21);
+            EncodingLabel.TabIndex = 5;
+            EncodingLabel.Text = "Encoding";
             // 
-            // label2
+            // DecodingLabel
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 12F);
-            label2.Location = new Point(467, 90);
-            label2.Name = "label2";
-            label2.Size = new Size(76, 21);
-            label2.TabIndex = 6;
-            label2.Text = "Decoding";
+            DecodingLabel.AutoSize = true;
+            DecodingLabel.Font = new Font("Segoe UI", 12F);
+            DecodingLabel.Location = new Point(473, 91);
+            DecodingLabel.Name = "DecodingLabel";
+            DecodingLabel.Size = new Size(76, 21);
+            DecodingLabel.TabIndex = 6;
+            DecodingLabel.Text = "Decoding";
+            // 
+            // DecodingMessage
+            // 
+            DecodingMessage.Enabled = false;
+            DecodingMessage.Location = new Point(412, 193);
+            DecodingMessage.Multiline = true;
+            DecodingMessage.Name = "DecodingMessage";
+            DecodingMessage.PlaceholderText = "message...";
+            DecodingMessage.ReadOnly = true;
+            DecodingMessage.Size = new Size(196, 112);
+            DecodingMessage.TabIndex = 7;
+            // 
+            // OpenEncodeFile
+            // 
+            OpenEncodeFile.Location = new Point(353, 125);
+            OpenEncodeFile.Name = "OpenEncodeFile";
+            OpenEncodeFile.Size = new Size(27, 25);
+            OpenEncodeFile.TabIndex = 8;
+            OpenEncodeFile.Text = ">";
+            OpenEncodeFile.UseVisualStyleBackColor = true;
+            OpenEncodeFile.Click += OpenEncodeFile_Click;
+            // 
+            // OpenDecodeFile
+            // 
+            OpenDecodeFile.Location = new Point(581, 125);
+            OpenDecodeFile.Name = "OpenDecodeFile";
+            OpenDecodeFile.Size = new Size(27, 25);
+            OpenDecodeFile.TabIndex = 9;
+            OpenDecodeFile.Text = ">";
+            OpenDecodeFile.UseVisualStyleBackColor = true;
+            OpenDecodeFile.Click += OpenDecodeFile_Click;
+            // 
+            // EncodeImageName
+            // 
+            EncodeImageName.AutoSize = true;
+            EncodeImageName.Location = new Point(184, 152);
+            EncodeImageName.Name = "EncodeImageName";
+            EncodeImageName.Size = new Size(0, 15);
+            EncodeImageName.TabIndex = 10;
+            // 
+            // DecodeImageName
+            // 
+            DecodeImageName.AutoSize = true;
+            DecodeImageName.Location = new Point(413, 152);
+            DecodeImageName.Name = "DecodeImageName";
+            DecodeImageName.Size = new Size(0, 15);
+            DecodeImageName.TabIndex = 11;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(textBox3);
-            Controls.Add(encodeMessage);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(encodingPath);
+            Controls.Add(DecodeImageName);
+            Controls.Add(EncodeImageName);
+            Controls.Add(OpenDecodeFile);
+            Controls.Add(OpenEncodeFile);
+            Controls.Add(DecodingMessage);
+            Controls.Add(DecodingLabel);
+            Controls.Add(EncodingLabel);
+            Controls.Add(DecodingPath);
+            Controls.Add(EncodingMessage);
+            Controls.Add(DecodeButton);
+            Controls.Add(EncodeButton);
+            Controls.Add(EncodingPath);
             Name = "Form1";
-            Text = "Form1";
+            Text = "        ";
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-        private TextBox encodingPath;
-        private Button button1;
-        private Button button2;
-        private TextBox encodeMessage;
-        private TextBox textBox3;
-        private Label label1;
-        private Label label2;
+        private TextBox EncodingPath;
+        private Button EncodeButton;
+        private Button DecodeButton;
+        private TextBox EncodingMessage;
+        private TextBox DecodingPath;
+        private Label EncodingLabel;
+        private Label DecodingLabel;
+        private TextBox DecodingMessage;
+        private Button OpenEncodeFile;
+        private Button OpenDecodeFile;
+        private Label EncodeImageName;
+        private Label DecodeImageName;
     }
 }
--- Forms/Form1.cs
+++ Forms/Form1.cs
@@ -2,34 +2,33 @@ namespace ImageSteganography
 {
     public partial class Form1 : Form
     {
+        public string EncodeFilePath, DecodeFilePath;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void OpenEncodeFile_Click(object sender, EventArgs e)
         {
-
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                EncodeFilePath = fileDialog.FileName;
+                EncodingPath.Text = fileDialog.FileName;
+                EncodeImageName.Text = fileDialog.SafeFileName;
+            }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void OpenDecodeFile_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                DecodeFilePath = fileDialog.FileName;
+                DecodingPath.Text = fileDialog.FileName;
+                DecodeImageName.Text = fileDialog.SafeFileName;
+            }
         }
     }
 }
